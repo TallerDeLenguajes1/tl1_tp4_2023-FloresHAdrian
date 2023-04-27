583a02468660ac2a8c879210674010bbd2ba19af
FHAdrian <floreshadrian@gmail.com>
2023-04-27 17:41:42 -0300

    buscar tarea por una palabra

--- tp4_2.c
+++ tp4_2.c
@@ -14,12 +14,15 @@ void inicializarTareas( tarea ** tareas, int n);
 void ingresarTareas(tarea ** tareas, int n);
 void controlTareasRealizadas( tarea ** tareasPendientes, tarea ** tareasRealizadas,int n);
 void mostrar( tarea ** tareas, int n);
+tarea buscarTarea( tarea ** tareasPendientes,tarea ** tareasRealizadas,int n, char * palabra);
+
 
 int main(int argc, char const *argv[])
 {
     int n;
     tarea ** tareasPendientes;
     tarea ** tareasRealizadas;
+    tarea tareaAux;
     printf("\n\nIngresar el numero de tareas a realizar: ");
     scanf("%d",&n);
     tareasPendientes =(tarea**) malloc(sizeof(tarea*)*n);//Le hago un sizof poirque el puntero struct es donde es posible que se a estar al estructura
@@ -29,9 +32,22 @@ int main(int argc, char const *argv[])
     inicializarTareas(tareasRealizadas,n);
     ingresarTareas(tareasPendientes,n);
     controlTareasRealizadas(tareasPendientes,tareasRealizadas,n);
-    mostrar(tareasPendientes,n);
+    // mostrar(tareasPendientes,n);
     // mostrar(tareasRealizadas,n);
 
+    printf("\n****Busco Tarea por palabra****\n");
+    fflush(stdin);
+    char *palabra;
+    palabra = (char * ) malloc (100*sizeof(char));
+    gets(palabra);
+    tareaAux = buscarTarea(tareasPendientes,tareasRealizadas,n,palabra);
+    if(tareaAux.TareaId==-1){
+        printf("\nNo se encontro ninguna tarea que contanga esa palabra");
+    }
+    else{
+        printf(" %s",tareaAux.Descripcion);
+
+    }
 
 
     // Libero la memoria dinamica?
@@ -108,5 +124,24 @@ void mostrar(tarea ** tareas, int n){
             printf("%s\n",tareas[i]->Descripcion);
         }
     }
+    
+}
+
+tarea buscarTarea( tarea ** tareasPendientes,tarea ** tareasRealizadas,int n, char * palabra){
+
+
+    for (int i = 0; i < n; i++)
+    {
+        if(tareasPendientes[i] != NULL && strstr( tareasPendientes[i]->Descripcion, palabra )){
+            return *tareasPendientes[i];
+        }
+        if(tareasRealizadas[i] !=NULL && strstr( tareasRealizadas[i]->Descripcion, palabra)){
+            return *tareasRealizadas[i];
+        }
+    }
+
+    tarea aux;
+    aux.TareaId=-1;  
+    return aux;
     
 }
