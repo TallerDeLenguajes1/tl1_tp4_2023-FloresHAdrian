b92d8b5c5c3fafcb356308846794f20b19c2c718
FHAdrian <floreshadrian@gmail.com>
2023-04-27 18:49:01 -0300

    Agregado menu de busqueda

--- tp4_2.c
+++ tp4_2.c
@@ -4,59 +4,59 @@
 
 struct Tarea
 {
-    int TareaId;  //Numerado en ciclo iterativo
+    int TareaId; // Numerado en ciclo iterativo
     char *Descripcion;
-    int Duracion; //entre 10-100
-}typedef tarea;
+    int Duracion; // entre 10-100
+} typedef tarea;
 
-
-void inicializarTareas( tarea ** tareas, int n);
-void ingresarTareas(tarea ** tareas, int n);
-void controlTareasRealizadas( tarea ** tareasPendientes, tarea ** tareasRealizadas,int n);
+void inicializarTareas(tarea **tareas, int n);
+void ingresarTareas(tarea **tareas, int n);
+void controlTareasRealizadas(tarea **tareasPendientes, tarea **tareasRealizadas, int n);
 void mostrarTarea(tarea tarea);
-void mostrar( tarea ** tareas, int n);
-tarea buscarTareaPorPalabra( tarea ** tareasPendientes,tarea ** tareasRealizadas,int n, char * palabra);
-
-tarea buscarTareaPorId( tarea **tareasPendientes, tarea **tareasRealizadas,int n);
+void mostrar(tarea **tareas, int n);
+tarea buscarTareaPorPalabra(tarea **tareasPendientes, tarea **tareasRealizadas, int n, char *palabra);
+tarea buscarTareaPorId(tarea **tareasPendientes, tarea **tareasRealizadas, int n, int id);
+void menu(tarea **tareasPendientes, tarea **tareasRealizadas, int n);
 
 int main(int argc, char const *argv[])
 {
     int n;
-    tarea ** tareasPendientes;
-    tarea ** tareasRealizadas;
+    tarea **tareasPendientes;
+    tarea **tareasRealizadas;
     tarea tareaAux;
     tarea tareaBuscada;
     printf("\n\nIngresar el numero de tareas a realizar: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    tareasPendientes =(tarea**) malloc(sizeof(tarea*)*n);//Le hago un sizof poirque el puntero struct es donde es posible que se a estar al estructura
-                                                // Va el sizeof va devolver el tamaño de un puntero
-    tareasRealizadas= (tarea**) malloc(sizeof(tarea*)*n); 
+    tareasPendientes = (tarea **)malloc(sizeof(tarea *) * n); // Le hago un sizof poirque el puntero struct es donde es posible que se a estar al estructura
+                                                              //  Va el sizeof va devolver el tamaño de un puntero
+    tareasRealizadas = (tarea **)malloc(sizeof(tarea *) * n);
 
-    inicializarTareas(tareasPendientes,n);
-    inicializarTareas(tareasRealizadas,n);
-    ingresarTareas(tareasPendientes,n);
-    controlTareasRealizadas(tareasPendientes,tareasRealizadas,n);
+    inicializarTareas(tareasPendientes, n);
+    inicializarTareas(tareasRealizadas, n);
+    ingresarTareas(tareasPendientes, n);
+    controlTareasRealizadas(tareasPendientes, tareasRealizadas, n);
+
+    menu(tareasPendientes,tareasRealizadas,n);
     // mostrar(tareasPendientes,n);
     // mostrar(tareasRealizadas,n);
-    // buscarTarea(tareasPendientes,tareasRealizadas,n);
-    tareaBuscada = buscarTareaPorId(tareasPendientes,tareasRealizadas,n);
-    mostrarTarea(tareaBuscada);
+    // tareaBuscada = buscarTareaPorId(tareasPendientes,tareasRealizadas,n);
+    // mostrarTarea(tareaBuscada);
 
-    printf("\n****Busco Tarea por palabra****\n");
-    fflush(stdin);
-    char *palabra;
-    palabra = (char * ) malloc (100*sizeof(char));
-    gets(palabra);
-    tareaAux = buscarTareaPorPalabra(tareasPendientes,tareasRealizadas,n,palabra);
-    if(tareaAux.TareaId==-1){
-        printf("\nNo se encontro ninguna tarea que contanga esa palabra");
-    }
-    else{
-        printf(" %s",tareaAux.Descripcion);
-
-    }
-
+    // printf("\n****Busco Tarea por palabra****\n");
+    // fflush(stdin);
+    // char *palabra;
+    // palabra = (char *)malloc(100 * sizeof(char));
+    // gets(palabra);
+    // tareaAux = buscarTareaPorPalabra(tareasPendientes, tareasRealizadas, n, palabra);
+    // if (tareaAux.TareaId == -1)
+    // {
+    //     printf("\nNo se encontro ninguna tarea que contanga esa palabra");
+    // }
+    // else
+    // {
+    //     printf(" %s", tareaAux.Descripcion);
+    // }
 
     // Libero la memoria dinamica?
     //*** Libero la memoria dinamica?***
@@ -76,103 +76,150 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-void inicializarTareas( tarea ** tareas, int n){
+void inicializarTareas(tarea **tareas, int n)
+{
     for (int i = 0; i < n; i++)
     {
         tareas[i] = NULL;
     }
 }
 
-void ingresarTareas(tarea ** tareas, int n){
+void ingresarTareas(tarea **tareas, int n)
+{
     char *Buff;
-    int dur=0;
+    int dur = 0;
 
-    Buff= (char*)malloc(sizeof(char) * 250);
+    Buff = (char *)malloc(sizeof(char) * 250);
     for (int i = 0; i < n; i++)
     {
         fflush(stdin);
         tareas[i] = malloc(sizeof(tarea));
-        tareas[i]->TareaId=i;
+        tareas[i]->TareaId = i;
         printf("\nIngresar duracion:");
-        scanf("%d",&dur);
-        fflush(stdin);     
-        tareas[i]->Duracion=dur;
+        scanf("%d", &dur);
+        fflush(stdin);
+        tareas[i]->Duracion = dur;
         printf("\nDescripcion de la Tarea: ");
         gets(Buff);
         fflush(stdin);
-        tareas[i]->Descripcion = (char*)malloc(sizeof(char)*strlen(Buff+1));
-        strcpy(tareas[i]->Descripcion,Buff);
-        
-
+        tareas[i]->Descripcion = (char *)malloc(sizeof(char) * strlen(Buff + 1));
+        strcpy(tareas[i]->Descripcion, Buff);
     }
     free(Buff);
-    
 }
 
+void controlTareasRealizadas(tarea **tareasPendientes, tarea **tareasRealizadas, int n)
+{
 
-void controlTareasRealizadas( tarea ** tareasPendientes, tarea ** tareasRealizadas,int n){
-
-    int aux=0,j=0;
+    int aux = 0, j = 0;
 
     for (int i = 0; i < n; i++)
     {
-        printf("\nEsta realizada la tarea %d, indique con SI=1 , NO=0: ",tareasPendientes[i]->TareaId+1);
-        scanf("%d",&aux);
-        if(aux==1){
+        printf("\nEsta realizada la tarea %d, indique con SI=1 , NO=0: ", tareasPendientes[i]->TareaId + 1);
+        scanf("%d", &aux);
+        if (aux == 1)
+        {
             tareasRealizadas[j] = tareasPendientes[i];
-            tareasPendientes[i]= NULL;
+            tareasPendientes[i] = NULL;
             j++;
         }
     }
 }
 
-void mostrarTarea( tarea tarea){
+void mostrarTarea(tarea tarea)
+{
 
-    printf("\nDescripcion: %s",tarea.Descripcion);
-    printf("\nDuracion: %d",tarea.Duracion);
+    printf("\nDescripcion: %s", tarea.Descripcion);
+    printf("\nDuracion: %d", tarea.Duracion);
 }
 
-void mostrar(tarea ** tareas, int n){
+void mostrar(tarea **tareas, int n)
+{
     for (int i = 0; i < n; i++)
     {
-        if(tareas[i]!=NULL){
-            printf("%s\n",tareas[i]->Descripcion);
+        if (tareas[i] != NULL)
+        {
+            printf("%s\n", tareas[i]->Descripcion);
         }
     }
-    
 }
 
-tarea buscarTareaPorPalabra( tarea ** tareasPendientes,tarea ** tareasRealizadas,int n, char * palabra){
-
+tarea buscarTareaPorPalabra(tarea **tareasPendientes, tarea **tareasRealizadas, int n, char *palabra)
+{
 
     for (int i = 0; i < n; i++)
     {
-        if(tareasPendientes[i] != NULL && strstr( tareasPendientes[i]->Descripcion, palabra )){
+        if (tareasPendientes[i] != NULL && strstr(tareasPendientes[i]->Descripcion, palabra))
+        {
             return *tareasPendientes[i];
         }
-        if(tareasRealizadas[i] !=NULL && strstr( tareasRealizadas[i]->Descripcion, palabra)){
+        if (tareasRealizadas[i] != NULL && strstr(tareasRealizadas[i]->Descripcion, palabra))
+        {
             return *tareasRealizadas[i];
         }
     }
 
     tarea aux;
-    aux.TareaId=-1;  
+    aux.TareaId = -1;
     return aux;
 }
-tarea buscarTareaPorId( tarea ** tareasPendients, tarea ** tareasRealizas, int n){
-    int id=0;
-    printf("\n\n --Ingresar el id que se busca entre 0 y %d: ",n-1);
-    scanf("%d",&id);
+tarea buscarTareaPorId(tarea **tareasPendients, tarea **tareasRealizas, int n, int id)
+{
+    // int id=0;
+    // printf("\n\n --Ingresar el id que se busca entre 0 y %d: ",n-1);
+    // scanf("%d",&id);
     for (int i = 0; i < n; i++)
     {
-        if(tareasPendients[i]!=NULL &&  tareasPendients[i]->TareaId == id){
+        if (tareasPendients[i] != NULL && tareasPendients[i]->TareaId == id)
+        {
             return *tareasPendients[i];
-            
         }
-        if(tareasRealizas[i]!=NULL &&  tareasRealizas[i]->TareaId == id){
-            return *tareasRealizas[i];        }
+        if (tareasRealizas[i] != NULL && tareasRealizas[i]->TareaId == id)
+        {
+            return *tareasRealizas[i];
+        }
     }
     tarea aux;
     aux.TareaId = -1;
     return aux;
+}
+
+void menu(tarea **tareasPendientes, tarea **tareasRealizadas, int n)
+{
+    int opc, id;
+    char * palabra;
+    tarea tareaAux;
+    printf("\n\n\n---------MENU DE BUSQUEDA---------\n");
+    printf("--Elija una opcion--\n");
+    printf("1.Busqueda por id.\n");
+    printf("2.Busqueda por palabra.\n");
+    // printf("0.Salir\n");
+    scanf("%d", &opc);
+    switch (opc)
+    {
+    case 1:
+        printf("Ingrese id que desesa buscar\n");
+        scanf("%d", &id);
+        tareaAux = buscarTareaPorId(tareasPendientes, tareasRealizadas, n, id);
+        break;
+    case 2:
+        printf("Ingrese palabra que buscar:\n");
+        fflush(stdin);
+        char *palabra;
+        palabra = (char *)malloc(100 * sizeof(char));
+        gets(palabra);
+        tareaAux = buscarTareaPorPalabra(tareasPendientes, tareasRealizadas, n, palabra);
+    default:
+        printf("Opcion ingresada no valida");
+        break;
+    }
+    if (tareaAux.TareaId == -1)
+    {
+        printf("\nNo se encontro ninguna tarea que contanga esa palabra");
+    }
+    else
+    {
+        mostrarTarea(tareaAux);
+    }
+
 }
